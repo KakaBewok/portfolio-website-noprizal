dd44cd3517fe1257f381d757c317e910f07795db
kkbwk <rizalnov667@gmail.com>
2023-02-04 17:16:35 +0700

    project page

--- src/components/titleProject.js
+++ src/components/titleProject.js
@@ -2,12 +2,19 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/globalContext';
 
 const TitleProject = () => {
-  const { locale } = useContext(GlobalContext);
+  const { locale, projects } = useContext(GlobalContext);
 
   return (
-    <h2 className="font-semibold text-3xl text-gray-700 dark:text-white mb-4 md:mb-0">
-      {locale === 'ID' ? 'Proyek Saya' : 'My Projects'}
-    </h2>
+    <>
+      <div className="flex justify-center items-start gap-1 flex-col mb-10 md:mb-0">
+        <h2 className="font-semibold text-3xl text-gray-700 dark:text-white ">
+          {locale === 'ID' ? 'Proyek Saya' : 'My Projects'}
+        </h2>
+        <p className="dark:text-gray-100 text-sm">
+          Number of projects: {projects.length}
+        </p>
+      </div>
+    </>
   );
 };
 
