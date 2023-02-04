ae82d60bb3318d3e016ea69923246b72c8901e14
kkbwk <rizalnov667@gmail.com>
2023-02-04 18:11:11 +0700

    fixed hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/globalContext';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   const { locale } = useContext(GlobalContext);
@@ -23,18 +24,22 @@ const Hero = () => {
             </h2>
 
             <div className="flex gap-5 items-center justify-center mt-4">
-              <a
-                href="#"
-                className="px-4 py-2 my-2 text-gray-100 uppercase bg-gray-800 border-2 border-gray-800 md:mt-16 dark:bg-white dark:text-gray-800 dark:border-0 hover:dark:text-gray-100 hover:dark:bg-gray-800  hover:bg-gray-100 hover:text-gray-800 text-sm md:text-md"
-              >
-                {locale === 'ID' ? 'PROYEK SAYA' : 'MY PROJECTS'}
-              </a>
-              <a
-                href="#"
-                className="px-6 py-2 my-2 text-gray-800 uppercase bg-transparent border-2 border-gray-800 md:mt-16 dark:bg-gray-800 dark:text-gray-100 hover:dark:bg-gray-100 hover:dark:text-gray-800 dark:border-0 hover:bg-gray-800 hover:text-white text-sm md:text-md"
-              >
-                {locale === 'ID' ? 'TENTANG SAYA' : 'ABOUT ME'}
-              </a>
+              <Link to="/project">
+                <a
+                  href="#"
+                  className="px-4 py-2 my-2 text-gray-100 uppercase bg-gray-800 border-2 border-gray-800 md:mt-16 dark:bg-white dark:text-gray-800 dark:border-0 hover:dark:text-gray-100 hover:dark:bg-gray-800  hover:bg-gray-100 hover:text-gray-800 text-sm md:text-md"
+                >
+                  {locale === 'ID' ? 'PROYEK SAYA' : 'MY PROJECTS'}
+                </a>
+              </Link>
+              <Link to="/about">
+                <a
+                  href="#"
+                  className="px-6 py-2 my-2 text-gray-800 uppercase bg-transparent border-2 border-gray-800 md:mt-16 dark:bg-gray-800 dark:text-gray-100 hover:dark:bg-gray-100 hover:dark:text-gray-800 dark:border-0 hover:bg-gray-800 hover:text-white text-sm md:text-md"
+                >
+                  {locale === 'ID' ? 'TENTANG SAYA' : 'ABOUT ME'}
+                </a>
+              </Link>
             </div>
           </div>
         </div>
